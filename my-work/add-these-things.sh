cf8eb24abbbd94123f3f9f4c38b5409229f17d47
Teagan Britten <teaganabritten@gmail.com>
2024-09-19 11:24:32 -0400

    Adding a python file to test

---
+++ my-work/add-these-things.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+echo "$0 - is the 0 argument"
+echo "$1 - is the 1 argument"
+echo "$2 - is the 2 argument"
---
+++ my-work/echo-this.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# Echo out something
+
+VAR1="Chocolate"
+
+echo "Hey $USER I see that your favourite flavour of Ice Cream is $VAR1"
+
