91232f8422c8865d34f6c767bc860192aebcfec2
Teagan Britten <teaganabritten@gmail.com>
2024-09-22 23:40:23 -0400

    edited second script

--- my-work/lab3/convert-bundle.sh
+++ my-work/lab3/convert-bundle.sh
@@ -2,14 +2,18 @@
 
 wget -c https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
 
-tar -xzf lab3-bundle.tar.gz > lab3bundle.tsv
+tar -xzf lab3-bundle.tar.gz
 
-awk '!/^[[:space:]]*$/' lab3bundle.tsv
+mv lab3-bundle.tar.gz lab3-bundle.tsv
 
-tr '\t' ',' < lab3bundle.tsv > lab3bundle.csv
+cat lab3-bundle | tr -s '\n' > lab3bundle.tsv
 
-wc -l lab3bundle.csv
+sed 's/'$'\t''/,/g' lab3bundle.tsv > lab3bundle.csv
+
+LINECOUNT=wc -l lab3bundle.csv
 
 echo $LINECOUNT
 
 tar -czf converted-archive.tar.gz lab3bundle.csv
+
+cat converted-archive.tar.gz
