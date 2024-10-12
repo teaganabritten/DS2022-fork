75039762cb428b5ad817322f2d7ab67e2a424d57
Teagan Britten <teaganabritten@gmail.com>
2024-10-11 23:42:36 -0400

    added lab5 script

---
+++ my-work/lab5/lab5script.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+/usr/bin/apt update -y
+/usr/bin/apt upgrade -y
+/usr/bin/apt install -y git
+/usr/bin/apt install -y jq
+/usr/bin/apt install -y python3
+/usr/bin/python3 -m pip install boto3
+/usr/bin/python3 -m pip install numpy
+
